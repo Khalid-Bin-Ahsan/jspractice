d195578720d14185583e53157ade9298ec20d680
Khalid-Bin-Ahsan <kbinahsan@gmail.com>
2026-01-31 00:23:27 +0600

    ternary 2

--- ternary.js
+++ ternary.js
@@ -16,3 +16,20 @@ condition ? do something when true   : do something when false
 */
 
 age >= 19 ? console.log("You can vote") : console.log("You go sleep")
+
+
+let price =700;
+const isLeader= false;
+
+price = isLeader=== true ? 0 : price + 100;
+console.log(price)
+
+
+// another example 
+
+
+price = isLeader=== true ? 
+    price > 1000 ? 
+        price= price/2 :  0 
+            : price + 1000;
+             console.log(price)
