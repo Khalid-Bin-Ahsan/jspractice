ee77fd5c718c1c012ebc1d50436674e3fa30afe1
Khalid-Bin-Ahsan <kbinahsan@gmail.com>
2026-02-04 20:04:46 +0600

    string

---
+++ string-object.js
@@ -0,0 +1,10 @@
+const country = 'Bangladesh' ;
+const division = "Dhaka";
+const district = `Satkhira`;
+const thana = new String ('Cantonment');
+
+console.log(typeof country);
+console.log(typeof country);
+console.log(typeof district);
+console.log(typeof thana);
+console.log(thana);
