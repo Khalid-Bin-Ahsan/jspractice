404d931fcbdf67b8c7963ad5c6cc17471f1aace5
Khalid-Bin-Ahsan <kbinahsan@gmail.com>
2026-02-05 14:53:27 +0600

    stcmp

---
+++ stcmp.js
@@ -0,0 +1,15 @@
+let school = 'Khulna Public College'
+
+let book = 'Physics'
+let sub = 'Chemistry'
+
+
+if (book === sub)
+{
+    console.log('Eibar kop hobe');   
+}
+
+else 
+{
+    console.log('Eibar kop khabo');
+}   
--- string-object.js
+++ string-object.js
@@ -8,3 +8,14 @@ console.log(typeof country);
 console.log(typeof district);
 console.log(typeof thana);
 console.log(thana);
+
+
+
+let team = 11;
+
+for ( var i = 0 ; i<=team; i++){
+console.log("this is team member ",i );
+}
+
+
+
